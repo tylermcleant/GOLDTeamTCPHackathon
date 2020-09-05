69dc2e3c59aa2b8462c99ef02f52d9beb8f9c644
nvansturgill <vanburen.nia@gmail.com>
2020-09-05 13:39:19 -0400

    Commit to turn in

--- index.js
+++ index.js
@@ -8,29 +8,45 @@ const app = express();
 // Establish connection to MongoDB
 const connectionString = 'mongodb+srv://space-place-admin:y8opHFb6aR7f@space-place.eqtit.azure.mongodb.net/space-place?retryWrites=true&w=majority';
 
+
 MongoClient.connect(connectionString, {
         useUnifiedTopology: true
-    },
-    function (err, client) {
-        if (err) {
-            return console.error(err);
-        } else {
-            console.log('Application now connected to MongoDB');
-            const db = client.db('space-place')
-            const usersCollection = db.collection('users');
+    })
+    .then(client => {
+        console.log('Application now connected to MongoDB');
+        const db = client.db('space-place');
+        const usersCollection = db.collection('users')
 
+        // Middleware
+        app.set('view engine', 'html')
+        app.use(bodyParser.urlencoded({
+            extended: true
+        }))
+        app.use(bodyParser.json())
+        app.use(express.static('public'))
 
-            // app.use( /* ... */ )
-            // app.post();
-            // app.listen( /* ... */ )
-        }
+        const port = 3000;
+        app.listen(port, function () {
+            console.log(`Server started and listening on ${port}`);
+            console.log(`Current build is viewable at http://localhost:${port}/`);
+        });
+
+        // HTML Route
+        app.get('/', (req, res) => {
+            db.collection('username')
+            res.render('index.html', {})
+        });
+
+        app.post('/username', (req, res) => {
+            usersCollection.insertOne(req.body)
+                .then(result => {
+                    console.log(result)
+                    res.render('index.html', {})
+                })
+                .catch(error => console.error(error))
+        })
     });
 
-const port = 3000;
-app.listen(port, function () {
-    console.log(`Server started and listening on ${port}`);
-    console.log(`Current build is viewable at http://localhost:${port}/`);
-});
 
 app.use(bodyParser.urlencoded({
     extended: true
